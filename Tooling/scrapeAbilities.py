1a4eec8ef12d65fb961205f4d3a4b7f3b943b043
anr002 <anr002@ucsd.edu>
2023-12-29 13:01:13 -0800

    Added ability icons

--- Tooling/scrapeAbilities.py
+++ Tooling/scrapeAbilities.py
@@ -9,20 +9,24 @@ base_url = "https://smite.fandom.com"
 with open('gods.json', 'r') as f:
     gods_list = json.load(f)
 
-ability_names = ["passive", "ability_1", "ability_2", "ability_3", "ability_4", "ability_5", "ability_6", "ability_7", "ability_8", "ability_9", "ability_10"]
+ability_names = ["passive"] + [f"ability_{i}" for i in range(1, 13)]
 
 for god in gods_list:
-    god_url = base_url + "/wiki/" + god['name'].replace(" ", "_")
+    god_name = god['name']
+    if god_name == "Chang E":
+        god_name = "Chang'e"
+    god_url = base_url + "/wiki/" + god_name.replace(" ", "_").replace("'", "%27")
     response = requests.get(god_url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     img_tags = soup.find_all('img', {'src': re.compile(r'Icons_.*\.png')})
 
-    os.makedirs(god['name'], exist_ok=True)
+    os.makedirs(os.path.join("God Abilities", god['name']), exist_ok=True)
 
     for i, img in enumerate(img_tags):
+        if i >= len(ability_names):
+            break
         icon_url = img['src']
-
         icon_response = requests.get(icon_url)
-        with open(os.path.join(god['name'], f"{ability_names[i]}.png"), 'wb') as f:
+        with open(os.path.join("God Abilities", god['name'], f"{ability_names[i]}.png"), 'wb') as f:
             f.write(icon_response.content)
